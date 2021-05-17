9dd662b468e8989a12e52ff69f26b2fdddc32d1f
zono2010mx <sergiohalo2011@hotmail.com>
2021-05-16 19:55:49 -0500

    python4

---
+++ Day43.py
@@ -0,0 +1,5 @@
+n = int(input("Introduce la altura del triángulo (entero positivo): "))
+for i in range(1, n+1, 2):
+    for j in range(i, 0, -2):
+        print(j, end=" ")
+    print("")
---
+++ Day44.py
@@ -0,0 +1,5 @@
+key = "contraseña"
+password =""
+while password != key:
+    password = input("Introduce la contraseña: ")
+print("Contraseña correcta")
