f25bbc815edd2e4508110bc867950fa2f271e2eb
zono2010mx <sergiohalo2011@hotmail.com>
2021-05-19 15:06:32 -0500

    Python4

---
+++ Day50.py
@@ -0,0 +1,7 @@
+subjects = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
+scores = []
+for subject in subjects:
+    score = input("¿Qué nota has sacado en " + subject + "?")
+    scores.append(score)
+for i in range(len(subjects)):
+    print("En " + subjects[i] + " has sacado " + scores[i])
---
+++ Day52.py
@@ -0,0 +1,3 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for i in range(1, 11):
+    print(numbers[-i], end=", ")
