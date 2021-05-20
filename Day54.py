c093f341c831acc0d57e79e94e4f7af5815da133
zono2010mx <sergiohalo2011@hotmail.com>
2021-05-20 16:14:41 -0500

    Python4

---
+++ Day54.py
@@ -0,0 +1,5 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+for i in range(len(alphabet), 1, -1):
+    if i % 3 == 0:
+        alphabet.pop(i-1)
+print(alphabet)
