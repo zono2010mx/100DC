b87b052eaf5be3b4db839a5ac621202a5be59245
zono2010mx <sergiohalo2011@hotmail.com>
2021-05-24 12:59:38 -0500

    Python4

---
+++ Day56.py
@@ -0,0 +1,8 @@
+word = input("Introduce una palabra: ")
+vocals = ['a', 'e', 'i', 'o', 'u']
+for vocal in vocals: 
+    times = 0
+    for letter in word: 
+        if letter == vocal:
+            times += 1
+    print("La vocal " + vocal + " aparece " + str(times) + " veces")
