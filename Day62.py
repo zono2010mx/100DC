451e3976f5608d4e9d47d159491853e96b386ef9
zono2010mx <sergiohalo2011@hotmail.com>
2021-05-26 11:05:58 -0500

    Python4

---
+++ Day62.py
@@ -0,0 +1,6 @@
+nombre = input('¿Cómo te llamas? ')
+edad = input('¿Cuántos años tienes? ')
+direccion = input('¿Cuál es tu dirección? ')
+telefono = input('¿Cuál es tu número de teléfono? ')
+persona = {'nombre': nombre, 'edad': edad, 'direccion': direccion, 'telefono': telefono}
+print(persona['nombre'], 'tiene', persona['edad'], 'años, vive en', persona['direccion'], 'y su número de teléfono es', persona['telefono'])
