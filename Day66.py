f69f7e652109c413ca435aa32c11430d9ea4289a
zono2010mx <sergiohalo2011@hotmail.com>
2021-05-28 17:08:55 -0500

    Python4

---
+++ Day66.py
@@ -0,0 +1,8 @@
+persona = {}
+continuar = True
+while continuar:
+    clave = input('¿Qué dato quieres introducir? ')
+    valor = input(clave + ': ')
+    persona[clave] = valor
+    print(persona)
+    continuar = input('¿Quieres añadir más información (Si/No)? ') == "Si" 
