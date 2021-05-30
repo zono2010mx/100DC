da2ee6b59ed9e69e45ea089bdeb00c3875368799
zono2010mx <sergiohalo2011@hotmail.com>
2021-05-30 17:21:02 -0500

    Python4

---
+++ Day69.py
@@ -0,0 +1,18 @@
+facturas = {}
+cobrado = 0
+pendiente = 0
+more = ''
+while more != 'T':
+    if more == 'A':
+        clave = input('Introduce el número de la factura: ')
+        coste = float(input('Introduce el coste de la factura: '))
+        facturas[clave] = coste
+        pendiente += coste
+    if more == 'P':
+        clave = input('Introduce el número de la factura a pagar: ')
+        coste = facturas.pop(clave, 0)
+        cobrado += coste
+        pendiente -= coste
+    print('Recaudado:', cobrado)
+    print('Pendiente de cobro: ', pendiente)
+    more = input('¿Quieres añadir una nueva factura (A), pagarla (P) o terminar (T)? ')
