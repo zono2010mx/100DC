24ae5c937e9c057cec8c442777fcab2035e3b7b0
zono2010mx <sergiohalo2011@hotmail.com>
2021-06-03 17:14:28 -0500

    Python4

---
+++ Day73.py
@@ -0,0 +1,11 @@
+def greet(nombre):
+    """Función que muestra un saludo por pantalla.
+    Parámetros
+    nombre: Nombre del usuario
+    Devuelve el saludo ¡Hola nombre!.
+    """
+    print('¡Hola ' + nombre +'!')
+    return
+
+greet('Alf')
+greet('Python')
