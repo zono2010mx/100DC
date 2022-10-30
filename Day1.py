b921d8ddd4054f389afa7bec99202afe9bad96f3
El mishi vaquero <sergiohalo2011@hotmail.com>
2022-10-30 16:58:45 -0500

    Cambios suma

--- Day1.py
+++ Day1.py
@@ -1,6 +1,6 @@
 #Programa de dia 1
 
-print ("Suma basica en python") 
+print ("Suma basica en python, te mucho cuidado xd") 
 n1=int(input("Dame un numero: "))
 n2=int(input("Dame otro numero: "))
 suma=n1+n2
--- Day18.py
+++ Day18.py
@@ -1,3 +1,3 @@
-frase= input("Introduce una palabra: ")
+frase= input("Introduce una palabra cualquiera: ")
 vocal= input("Introduce una vocal minuscula: ")
 print(frase.replace(vocal, vocal.upper()))
